74c0936aa80855f4173620f9845d6a2de7974351
juniorbotelho <junowerther@gmail.com>
2022-09-05 11:29:25 -0300

    initial commit

---
+++ src/chat.js
@@ -0,0 +1,8 @@
+// Please use event listeners to run functions.
+document.addEventListener('onLoad', function(obj) {
+  // Disparado uma única vez quando o documeto é carregado
+});
+
+document.addEventListener('onEventReceived', function(obj) {
+	// Contém informações que podem ser manipuladas antes de serem exibidas
+});
